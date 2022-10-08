72482dba14d9fe8c3df324876fda042b3f5d8f56
Vaibhav Kharche <vaibhavmkharche@gmail.com>
2022-10-08 10:45:43 +0530

    Initial commit

---
+++ src/main/java/com/maveric/mavericecom/MavericEcomApplication.java
@@ -0,0 +1,15 @@
+package com.maveric.mavericecom;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class MavericEcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavericEcomApplication.class, args);
+	}
+
+}
